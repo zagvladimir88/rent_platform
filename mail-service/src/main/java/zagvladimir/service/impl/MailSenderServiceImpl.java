2f01be512c927868b559c06cfccab85ef169f4d3
zagvladimir88 <zagvladimir88@gmail.com>
2023-04-13 22:13:15 +0300

    added sl4j to email service

--- mail-service/src/main/java/zagvladimir/service/impl/MailSenderServiceImpl.java
+++ mail-service/src/main/java/zagvladimir/service/impl/MailSenderServiceImpl.java
@@ -1,6 +1,7 @@
 package zagvladimir.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -15,6 +16,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @PropertySource("classpath:mail.properties")
@@ -28,10 +30,12 @@ public class MailSenderServiceImpl implements MailSenderService {
     private final SpringTemplateEngine thymeleafTemplateEngine;
 
     public void sendMessageUsingThymeleafTemplate(MailParams mailParams) throws MessagingException {
+        log.info("Method sendMessageUsingThymeleafTemplate in MailSenderServiceImpl start");
         Context thymeleafContext = new Context();
         thymeleafContext.setVariables(mailParams.getTemplateModel());
         String htmlBody = thymeleafTemplateEngine.process("email-template.html", thymeleafContext);
         send(mailParams, htmlBody);
+        log.info("Method sendMessageUsingThymeleafTemplate in MailSenderServiceImpl finish");
     }
 
     public void sendConfirmBookingMail(MailParams mailParams) throws MessagingException {
@@ -43,6 +47,7 @@ public class MailSenderServiceImpl implements MailSenderService {
     }
 
     private void send(MailParams mailParams, String htmlBody) throws MessagingException {
+        log.info("Method send in MailSenderServiceImpl start");
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
         helper.setFrom(noreplyAddress);
@@ -55,5 +60,6 @@ public class MailSenderServiceImpl implements MailSenderService {
             helper.addAttachment("billing.pdf", file);
         }
         emailSender.send(message);
+        log.info("Email sent to {} successfully", mailParams.getEmailTo());
     }
 }
