3d83288f83f2b5e5a593f218f5d83694d0825a00
zagvladimir88 <zagvladimir88@gmail.com>
2023-04-13 19:08:10 +0300

    small refactoring

--- common-jpa/src/main/java/com/zagvladimir/dto/response/auth/MessageResponse.java
+++ common-jpa/src/main/java/com/zagvladimir/dto/response/auth/MessageResponse.java
@@ -1,17 +1,13 @@
 package com.zagvladimir.dto.response.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class MessageResponse {
   private String message;
 
-  public MessageResponse(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
 }
