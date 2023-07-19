5ad6502143098d4caccd0a375bd30be0845600cd
zagvladimir88 <zagvladimir88@gmail.com>
2023-07-19 23:23:41 +0300

    add kafka for mail service and refactor UserServiceImpl

--- common-jpa/src/main/java/com/zagvladimir/service/user/UserServiceImpl.java
+++ common-jpa/src/main/java/com/zagvladimir/service/user/UserServiceImpl.java
@@ -16,16 +16,15 @@ import com.zagvladimir.repository.ItemLeasedRepository;
 import com.zagvladimir.repository.RoleRepository;
 import com.zagvladimir.repository.UserRepository;
 import com.zagvladimir.service.pdf.PDFService;
+import org.springframework.kafka.core.KafkaTemplate;
 import zagvladimir.utils.UUIDGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 import zagvladimir.dto.MailParams;
 
 import javax.mail.MessagingException;
@@ -36,7 +35,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -45,232 +43,224 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-  private String mailServiceUri = "http://localhost:8086/mail/send";
-  private static final String ACTIVATION_URL =
-      "http://localhost:8080/api/auth/activate/%s/";
-  private final UserRepository userRepository;
-  private final RoleRepository roleRepository;
-  private final BCryptPasswordEncoder passwordEncoder;
-  private final PDFService pdfService;
-  private final ItemLeasedRepository itemLeasedRepository;
-  private final UserMapper userMapper;
+    private static final String ACTIVATION_URL =
+            "http://localhost:8080/api/auth/activate/%s/";
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final PDFService pdfService;
+    private final ItemLeasedRepository itemLeasedRepository;
+    private final UserMapper userMapper;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-  @Override
-  public List<UserResponse> findAll() {
-    return userRepository.findAll().stream().map(userMapper::toResponse).collect(Collectors.toList());
-  }
-
-  @Override
-  public Page<UserResponse> findAll(Pageable page) {
-    return userRepository.findAll(page).map(userMapper::toResponse);
-  }
-
-  @Transactional
-  @Override
-  public UserResponse create(UserCreateRequest request) throws MessagingException {
-    User user = userMapper.convertCreateRequest(request);
-    addRole(user, roleRepository.findRoleByName("ROLE_USER"));
-    user.setRegistrationDate(new Timestamp(new Date().getTime()));
-    user.getCredentials()
-        .setUserPassword(passwordEncoder.encode(user.getCredentials().getUserPassword()));
-
-    user.setStatus(Status.NOT_ACTIVE);
-    user.setActivationCode(UUIDGenerator.generatedUI());
-    userRepository.save(user);
-
-    if (userRepository.findUsersByCredentials_Email(user.getCredentials().getEmail()).isPresent()) {
-      sendRequestToMailService(user);
+    @Override
+    public List<UserResponse> findAll() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toResponse)
+                .collect(Collectors.toList());
     }
-    return userRepository.findById(user.getId()).map(userMapper::toResponse).orElseThrow(IllegalArgumentException::new);
-  }
 
-  @Override
-  public UserResponse findById(Long userId) {
-    return userRepository
-        .findById(userId).map(userMapper::toResponse)
-        .orElseThrow(
-            () ->
-                new EntityNotFoundException(
-                    String.format("The user with id:%d not found", userId)));
-  }
-
-  @Transactional
-  @Override
-  public UserResponse update(UserUpdateRequest request,Long id) {
-    Optional<User> user = userRepository.findById(id);
-    User updatedUser = null;
-
-    if(user.isPresent()) {
-      updatedUser = userMapper.convertUpdateRequest(request, user.get());
-    } else throw new EntityNotFoundException(String.format("User with ID: %d not found", id) );
-
-    updatedUser.setModificationDate(new Timestamp(new Date().getTime()));
-    updatedUser
-        .getCredentials()
-        .setUserPassword(passwordEncoder.encode(updatedUser.getCredentials().getUserPassword()));
-    userRepository.save(updatedUser);
-    return userRepository.findById(updatedUser.getId()).map(userMapper::toResponse).orElseThrow(EntityNotFoundException::new);
-  }
-
-  @Transactional
-  @Override
-  public UserResponse update(UserChangeCredentialsRequest request, Long id) {
-    Optional<User> user = userRepository.findById(id);
-    User updatedUser = null;
-    if(user.isPresent()) {
-      updatedUser = userMapper.convertUpdateRequest(request, user.get());
-    } else throw new EntityNotFoundException("User with ID: " + id + " not found");
-    updatedUser.setModificationDate(new Timestamp(new Date().getTime()));
-    updatedUser
-            .getCredentials()
-            .setUserPassword(passwordEncoder.encode(updatedUser.getCredentials().getUserPassword()));
-    userRepository.save(updatedUser);
-    return userRepository.findById(updatedUser.getId()).map(userMapper::toResponse).orElseThrow(EntityNotFoundException::new);
-  }
-
-  @Transactional
-  @Override
-  public UserResponse update(UserChangeAddressRequest request, Long id) {
-    Optional<User> user = userRepository.findById(id);
-    User updatedUser = null;
-    if(user.isPresent()) {
-      updatedUser = userMapper.convertUpdateRequest(request, user.get());
-    } else throw new EntityNotFoundException("User with ID: " + id + " not found");
-    updatedUser.setModificationDate(new Timestamp(new Date().getTime()));
-    updatedUser
-            .getCredentials()
-            .setUserPassword(passwordEncoder.encode(updatedUser.getCredentials().getUserPassword()));
-    userRepository.save(updatedUser);
-    return userRepository.findById(updatedUser.getId()).map(userMapper::toResponse).orElseThrow(EntityNotFoundException::new);
-  }
-
-  @Transactional
-  @Override
-  public Long delete(Long id) {
-    userRepository.deleteById(id);
-    return id;
-  }
-
-  @Override
-  public UserResponse findByLogin(String login) {
-    return userRepository
-        .findByCredentialsUserLogin(login)
-            .map(userMapper::toResponse)
-        .orElseThrow(
-            () ->
-                new EntityNotFoundException(
-                    String.format("The user with login: %s not found", login)));
-  }
-
-  @Override
-  public boolean activateUser(String code) {
-    Optional<User> user = userRepository.findUsersByActivationCode(code);
-    if (user.isPresent() && user.get().getStatus().equals(Status.NOT_ACTIVE)) {
-      user.get().setStatus(Status.ACTIVE);
-      userRepository.save(user.get());
-      return true;
+    @Override
+    public Page<UserResponse> findAll(Pageable page) {
+        return userRepository.findAll(page)
+                .map(userMapper::toResponse);
     }
-    return false;
-  }
 
-  @Override
-  public boolean isActivated(String login) {
-    Optional<User> user = userRepository.findByCredentialsUserLogin(login);
-    return user.map(value -> value.getStatus().equals(Status.ACTIVE)).orElse(false);
-  }
+    @Transactional
+    @Override
+    public UserResponse create(UserCreateRequest request) throws MessagingException {
+        User user = userMapper.convertCreateRequest(request);
+        addRole(user, roleRepository.findRoleByName("ROLE_USER"));
+        user.setRegistrationDate(new Timestamp(new Date().getTime()));
+        user.getCredentials()
+                .setUserPassword(passwordEncoder.encode(user.getCredentials().getUserPassword()));
 
-  @Override
-  public void confirmItemBooking(Long userID) throws MessagingException {
-    User user =
-        userRepository
-            .findById(userID)
-            .orElseThrow(
-                () ->
-                    new EntityNotFoundException(
-                        String.format("The User with id: %d not found", userID)));
+        user.setStatus(Status.NOT_ACTIVE);
+        user.setActivationCode(UUIDGenerator.generatedUI());
+        userRepository.save(user);
 
-    Set<ItemLeased> itemsLeasedSet = user.getItemsLeased();
-    String pathToBilling = pdfService.generateAnInvoice(itemsLeasedSet, user.getId());
+        if (userRepository.findUsersByCredentials_Email(user.getCredentials().getEmail()).isPresent()) {
+            sendRequestToMailService(user);
+        }
+        return userRepository.findById(user.getId()).map(userMapper::toResponse).orElseThrow(IllegalArgumentException::new);
+    }
 
-    Map<String, Object> templateModel = new HashMap<>();
-    templateModel.put("recipientName", user.getFirstName());
-    templateModel.put("email", user.getCredentials().getEmail());
-    templateModel.put("name", user.getFirstName());
-    templateModel.put("item_name", "item name");
+    @Override
+    public UserResponse findById(Long userId) {
+        return userRepository
+                .findById(userId).map(userMapper::toResponse)
+                .orElseThrow(
+                        () ->
+                                new EntityNotFoundException(
+                                        String.format("The user with id:%d not found", userId)));
+    }
+
+    @Transactional
+    @Override
+    public UserResponse update(UserUpdateRequest request, Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("User with ID: %d not found", id)));
+
+        userMapper.convertUpdateRequest(request, user);
+        user.setModificationDate(new Timestamp(System.currentTimeMillis()));
+        user.getCredentials().setUserPassword(passwordEncoder.encode(user.getCredentials().getUserPassword()));
+
+        User updatedUser = userRepository.save(user);
+        return userMapper.toResponse(updatedUser);
+    }
+
+    @Transactional
+    @Override
+    public UserResponse update(UserChangeCredentialsRequest request, Long id) {
+        User updatedUser = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with ID: " + id + " not found"));
+
+        userMapper.convertUpdateRequest(request, updatedUser);
+        updatedUser.setModificationDate(new Timestamp(new Date().getTime()));
+        updatedUser.getCredentials().setUserPassword(passwordEncoder.encode(updatedUser.getCredentials().getUserPassword()));
+
+        userRepository.save(updatedUser);
+        return userMapper.toResponse(updatedUser);
+    }
+
+    @Transactional
+    @Override
+    public UserResponse update(UserChangeAddressRequest request, Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with ID: " + id + " not found"));
+
+        User updatedUser = userMapper.convertUpdateRequest(request, user);
+        updatedUser.setModificationDate(new Timestamp(new Date().getTime()));
+
+        Credentials credentials = updatedUser.getCredentials();
+        credentials.setUserPassword(passwordEncoder.encode(credentials.getUserPassword()));
+
+        User savedUser = userRepository.save(updatedUser);
+        return userMapper.toResponse(savedUser);
+    }
+
+    @Override
+    public Long delete(Long id) {
+        userRepository.deleteById(id);
+        return id;
+    }
+
+    @Override
+    public UserResponse findByLogin(String login) {
+        return userRepository
+                .findByCredentialsUserLogin(login)
+                .map(userMapper::toResponse)
+                .orElseThrow(
+                        () ->
+                                new EntityNotFoundException(
+                                        String.format("The user with login: %s not found", login)));
+    }
+
+    @Transactional
+    @Override
+    public boolean activateUser(String code) {
+        User user = userRepository.findUsersByActivationCode(code)
+                .filter(u -> u.getStatus() == Status.NOT_ACTIVE)
+                .orElse(null);
+
+        if (user != null) {
+            user.setStatus(Status.ACTIVE);
+            userRepository.save(user);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isActivated(String login) {
+        return userRepository.findByCredentialsUserLogin(login)
+                .map(user -> user.getStatus() == Status.ACTIVE)
+                .orElse(false);
+    }
+
+    @Override
+    public void confirmItemBooking(Long userID) throws MessagingException {
+        User user =
+                userRepository
+                        .findById(userID)
+                        .orElseThrow(
+                                () ->
+                                        new EntityNotFoundException(
+                                                String.format("The User with id: %d not found", userID)));
+
+        Set<ItemLeased> itemsLeasedSet = user.getItemsLeased();
+        String pathToBilling = pdfService.generateAnInvoice(itemsLeasedSet, user.getId());
+
+        Map<String, Object> templateModel = new HashMap<>();
+        templateModel.put("recipientName", user.getFirstName());
+        templateModel.put("email", user.getCredentials().getEmail());
+        templateModel.put("name", user.getFirstName());
+        templateModel.put("item_name", "item name");
 
 //    mailSenderService.sendConfirmBookingMail(
 //        user.getCredentials().getEmail(), "Confirm Booking", pathToBilling, templateModel);
 
-    for (ItemLeased leased : itemsLeasedSet) {
-      leased.setStatus(Status.ACTIVE);
-      itemLeasedRepository.save(leased);
+        for (ItemLeased leased : itemsLeasedSet) {
+            leased.setStatus(Status.ACTIVE);
+            itemLeasedRepository.save(leased);
+        }
     }
-  }
 
-  @Override
-  public Long softDelete(Long userId) {
-    User toUpdate =
-        userRepository
-            .findById(userId)
-            .orElseThrow(
-                () ->
-                    new EntityNotFoundException(
-                        String.format("The user with id: %d not found", userId)));
-    toUpdate.setStatus(Status.DELETED);
-    userRepository.save(toUpdate);
-    return userId;
-  }
+    @Override
+    public Long softDelete(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("The user with id: " + userId + " not found"));
 
-  @Override
-  @Transactional
-  public void create(SignupRequest signUpRequest) throws MessagingException {
+        user.setStatus(Status.DELETED);
+        userRepository.save(user);
 
-    Credentials credentials = new Credentials();
-    credentials.setUserLogin(signUpRequest.getUserLogin());
-    credentials.setEmail(signUpRequest.getEmail());
-    credentials.setUserPassword(passwordEncoder.encode(signUpRequest.getUserPassword()));
-
-    User user = userMapper.convertSignUpRequest(signUpRequest);
-    user.setCredentials(credentials);
-
-    addRole(user, roleRepository.findRoleByName("ROLE_USER"));
-
-    user.setRegistrationDate(new Timestamp(new Date().getTime()));
-    user.setStatus(Status.NOT_ACTIVE);
-    user.setActivationCode(UUIDGenerator.generatedUI());
-    userRepository.save(user);
-
-    if (userRepository.findUsersByCredentials_Email(user.getCredentials().getEmail()).isPresent()) {
-      sendRequestToMailService(user);
+        return userId;
     }
-  }
 
-  private void addRole(User user, Role role) {
-    Set<Role> rolesList = new HashSet<>();
-    rolesList.add(role);
-    user.setRoles(rolesList);
-    role.getUsers().add(user);
-  }
+    @Override
+    @Transactional
+    public void create(SignupRequest signUpRequest) throws MessagingException {
 
-  private ResponseEntity<String> sendRequestToMailService(User user) {
-    RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
+        Credentials credentials = new Credentials();
+        credentials.setUserLogin(signUpRequest.getUserLogin());
+        credentials.setEmail(signUpRequest.getEmail());
+        credentials.setUserPassword(passwordEncoder.encode(signUpRequest.getUserPassword()));
 
-    Map<String, Object> templateModel = new HashMap<>();
-    templateModel.put("recipientName", user.getFirstName());
-    templateModel.put("email", user.getCredentials().getEmail());
-    templateModel.put("url", String.format(ACTIVATION_URL, user.getActivationCode()));
+        User user = userMapper.convertSignUpRequest(signUpRequest);
+        user.setCredentials(credentials);
 
-    MailParams mailParams = MailParams.builder()
-            .emailTo(user.getCredentials().getEmail())
-            .subject("Activation link")
-            .templateModel(templateModel)
-            .build();
-    var request = new HttpEntity<>(mailParams, headers);
-    return restTemplate.exchange(mailServiceUri,
-            HttpMethod.POST,
-            request,
-            String.class);
-  }
+        addRole(user, roleRepository.findRoleByName("ROLE_USER"));
+
+        user.setRegistrationDate(new Timestamp(new Date().getTime()));
+        user.setStatus(Status.NOT_ACTIVE);
+        user.setActivationCode(UUIDGenerator.generatedUI());
+        userRepository.save(user);
+
+        if (userRepository.findUsersByCredentials_Email(user.getCredentials().getEmail()).isPresent()) {
+            sendRequestToMailService(user);
+        }
+    }
+
+    private void addRole(User user, Role role) {
+        Set<Role> rolesList = new HashSet<>();
+        rolesList.add(role);
+        user.setRoles(rolesList);
+        role.getUsers().add(user);
+    }
+
+    private void sendRequestToMailService(User user) {
+        Map<String, Object> templateModel = new HashMap<>();
+        templateModel.put("recipientName", user.getFirstName());
+        templateModel.put("email", user.getCredentials().getEmail());
+        templateModel.put("url", String.format(ACTIVATION_URL, user.getActivationCode()));
+
+        MailParams mailParams = MailParams.builder()
+                .emailTo(user.getCredentials().getEmail())
+                .subject("Activation link")
+                .templateModel(templateModel)
+                .build();
+
+        kafkaTemplate.send("mail-topic", mailParams);
+    }
 }
---
+++ mail-service/src/main/java/zagvladimir/controller/Consumer.java
@@ -0,0 +1,20 @@
+package zagvladimir.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import zagvladimir.dto.MailParams;
+import zagvladimir.service.impl.MailSenderServiceImpl;
+
+import javax.mail.MessagingException;
+
+@Component
+@RequiredArgsConstructor
+public class Consumer {
+    private final MailSenderServiceImpl mailSenderServiceImpl;
+
+    @KafkaListener(topics = "mail-topic", groupId = "myGroup")
+    public void listenGroupFoo(MailParams message) throws MessagingException {
+        mailSenderServiceImpl.sendMessageUsingThymeleafTemplate(message);
+    }
+}
